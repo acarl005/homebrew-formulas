eb8a14e3f0d4e1c8541dcf94d9cd55ebad64275b
Andrew Carlson <acarl005@g.ucla.edu>
2018-06-12 16:19:05 -0700

    try different path

--- Formula/ls-go.rb
+++ Formula/ls-go.rb
@@ -10,7 +10,7 @@ class LsGo < Formula
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/acarl005").mkpath
     ln_s buildpath, buildpath/"src/github.com/acarl005/ls-go"
-    system "go", "get", "./"
+    system "go", "get", buildpath/"src/github.com/acarl005/ls-go"
     system "go", "build", "-o", bin/"ls-go"
   end
 
