e18afe8fe0832b88c249b2150e6bddc0f7b433c9
Andrew Carlson <acarl005@g.ucla.edu>
2018-06-12 07:15:10 -0700

    initial commit

---
+++ ls-go.rb
@@ -0,0 +1,20 @@
+class LsGo < Formula
+  desc "A more colorful, user-friendly implementation of `ls` written in Go"
+  homepage "https://github.com/acarl005/ls-go"
+  url "https://github.com/acarl005/ls-go/archive/v0.0.0.tar.gz"
+  sha256 "db9ba7300fbbaf92926b2c95fd63e3e936739e359f123b5a45e6ca04b490af51"
+  
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/acarl005").mkpath
+    ln_s buildpath, buildpath/"src/github.com/acarl005/ls-go"
+    system "go", "install"
+    system "go", "build", "-o", bin/"ls-go"
+  end
+
+  test do
+    system bin/"ls-go", "--help"
+  end
+end
