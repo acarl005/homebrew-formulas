8c3f3dbde98d528376c51012bb7c581cd1246c55
Andrew Carlson <acarl005@g.ucla.edu>
2019-08-18 16:11:28 -0400

    update ls-go

--- Formula/ls-go.rb
+++ Formula/ls-go.rb
@@ -1,8 +1,8 @@
 class LsGo < Formula
   desc "A more colorful, user-friendly implementation of `ls` written in Go"
   homepage "https://github.com/acarl005/ls-go"
-  url "https://github.com/acarl005/ls-go/archive/v0.0.14.tar.gz"
-  sha256 "566835e08fbaad603b0b372b48d49fbda8af0064e5a5a3bd9ab049701d48e077"
+  url "https://github.com/acarl005/ls-go/archive/v0.0.15.tar.gz"
+  sha256 "6606abf7eab3865a14022c2aa1a3c857bbb2a4abc4ad6b5b24dfdf51cd9f8e91"
   
   depends_on "go" => :build
 
