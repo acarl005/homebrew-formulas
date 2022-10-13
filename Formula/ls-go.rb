59f29a02a52a36d0b0b7f3dd067ff1ddb5f16d65
Andy Carlson <andy@warp.dev>
2022-10-13 15:34:01 -0700

    update ls-go

--- Formula/ls-go.rb
+++ Formula/ls-go.rb
@@ -1,8 +1,8 @@
 class LsGo < Formula
   desc "A more colorful, user-friendly implementation of `ls` written in Go"
   homepage "https://github.com/acarl005/ls-go"
-  url "https://github.com/acarl005/ls-go/archive/v0.2.2.tar.gz"
-  sha256 "83b893ac70ae9be6fd72e81e68ce2a5a2e2909663aab1f714301345a7142a014"
+  url "https://github.com/acarl005/ls-go/archive/v0.2.3.tar.gz"
+  sha256 "27f2bbb9cc4a9ceb0bbbb33e6e253d617c80114ef29950c8e2e1f6663edc01f0"
   
   depends_on "go" => :build
 
@@ -10,7 +10,7 @@ class LsGo < Formula
     ENV["GOPATH"] = buildpath
     (buildpath/"build/src").mkpath
     ln_s buildpath, buildpath/"build/src/github.com"
-    system "GOPATH=$PWD/build go install github.com/acarl005/ls-go@v0.2.2"
+    system "GOPATH=$PWD/build go install github.com/acarl005/ls-go@latest"
     bin.install "build/bin/ls-go"
   end
 
