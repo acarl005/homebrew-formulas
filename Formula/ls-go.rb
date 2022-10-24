0a41a9cc60c06145609133f13814ac60962cce41
Andy Carlson <andy@warp.dev>
2022-10-23 18:54:05 -0700

    update ls-go

--- Formula/ls-go.rb
+++ Formula/ls-go.rb
@@ -1,9 +1,9 @@
 class LsGo < Formula
   desc "A more colorful, user-friendly implementation of `ls` written in Go"
   homepage "https://github.com/acarl005/ls-go"
-  url "https://github.com/acarl005/ls-go/archive/v0.2.3.tar.gz"
-  sha256 "27f2bbb9cc4a9ceb0bbbb33e6e253d617c80114ef29950c8e2e1f6663edc01f0"
-  
+  url "https://github.com/acarl005/ls-go/archive/v0.2.5.tar.gz"
+  sha256 "aec7d819bbd13c03fd4d53f8009780a79eaeb2979ee14557d16975d3bb7638cf"
+
   depends_on "go" => :build
 
   def install
